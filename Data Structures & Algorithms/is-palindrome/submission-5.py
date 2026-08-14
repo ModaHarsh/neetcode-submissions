7b4de85cabdf23400bf8a3fec53ea11b65049179
Harsh <postharshmoda@gmail.com>
2026-08-14 11:22:51 +0530

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strList = []
+        for i in range(0,len(s)):
+            if s[i].isalnum():
+                strList.append(s[i].lower())
+        
+        i = 0
+
+        for i in range(0, int(len(strList)/2)):
+            if strList[i] != strList[len(strList)-1-i]:
+                return False
+        return True
+
+
+        
