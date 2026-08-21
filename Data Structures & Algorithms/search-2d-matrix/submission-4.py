27b76d4bbc21e513194e464cedc93d5e973a8f8c
Harsh <postharshmoda@gmail.com>
2026-08-21 10:14:15 +0530

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.py
@@ -0,0 +1,36 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        def findHeight(s, b):
+            l = len(matrix[0])
+            while(s<=b):
+                height = int((s + b)/2)
+                
+                if(target > matrix[height][len(matrix[0])-1]):
+                    s = height + 1
+                elif(target < matrix[height][0]):
+                    b = height - 1 
+                else: 
+                    return height
+            return None
+
+        height = findHeight(0, len(matrix)-1)
+        
+        if height == None:
+            return False
+        
+        l = 0
+        r = len(matrix[0]) - 1
+        while(l <= r):
+            mid = int((l + r)/2)
+            
+            if(target > matrix[height][mid]):
+                l = mid + 1
+            elif(target < matrix[height][mid]):
+                r = mid - 1
+            else:
+                return True
+        return False
+
+
+
+        
