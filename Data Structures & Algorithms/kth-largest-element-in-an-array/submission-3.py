d355d5f86774e42d3f9aac58734f47018bcd9b05
Harsh <postharshmoda@gmail.com>
2026-09-05 14:32:33 +0530

    Add: kth-largest-element-in-an-array - submission-3

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return heapq.nlargest(k, nums)[-1]        
+
+        ## heapq.nlargest(k, heap)
+        ## returns the n largest element in an array 
+        ## in a decreasing order
+        ## it forms a heap of length k internally 
+        ## by iterating though all n elements and then returns the
+        ## k largest elements
+
+        ## overall TC is O( n logk )
+        ## n elements to iterate for 
+        ## and k elements to be stored in the array
+
+        ## and SC is O(k) as no more than k elements 
+        ## are being stored at a time
